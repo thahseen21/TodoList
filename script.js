04847351472c2627652f5612ee63b1dfef39cfa6
thahseen21 <thahseen.niaz@gmail.com>
2021-08-02 18:28:18 +0530

    implemented markAsRead and to remove the task only after markAsRead

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ var todoList = [];
 
 function AddTodo() {
     const value = document.getElementById("add-todo").value;
+    if (todoList.length === 0) {
+        ToggleNoTodo()
+    }
     if (value) {
-        todoList.push(value)
+        let temp = { value: value, isRead: false }
+        todoList.push(temp)
         Display();
         document.getElementById("add-todo").value = null;
-    }
-    if (todoList.length > 0) {
-        ToggleNoTodo()
     }
 }
 
@@ -17,21 +18,32 @@ function Display() {
     p.className = `todo` // setting the <p> with a classname as 'todo'
     p.id = `${todoList.length - 1}`
 
-    const node = document.createTextNode(todoList[todoList.length - 1]); // creating a text
+    const removeBtn = document.createElement("button");
+    removeBtn.textContent = 'X';
+    removeBtn.addEventListener('click', () => RemoveTodo(p.id))
+
+    const node = document.createTextNode(todoList[todoList.length - 1].value); // creating a text
 
     p.appendChild(node); // adding the text to the <p>
-    p.addEventListener('click', RemoveTodo)
+    p.appendChild(removeBtn); // adding the text to the <p>
+    // p.addEventListener('click', RemoveTodo);
+    p.addEventListener('dblclick', () => MarkReadToggler(p.id));
 
     const element = document.getElementById("list-container"); // getting where to insert the <p> tag
     element.append(p);// appending p tag as child to the "list-container" id.
 }
 
 function RemoveTodo(id) {
-    let idName = id?.target?.id
-    todoList.splice(idName, 1)
-    let tag = document.getElementById(idName);
-    tag.remove();
-    ToggleNoTodo();
+    if (todoList[id].isRead) {
+        todoList.splice(id, 1)
+        let tag = document.getElementById(id);
+        tag.remove();
+    } else {
+        alert("Please finish the task to remove...")
+    }
+    if (todoList.length == 0) {
+        ToggleNoTodo();
+    }
 }
 
 function ToggleNoTodo() {
@@ -40,5 +52,14 @@ function ToggleNoTodo() {
         todo.style.display = 'block'
     } else {
         todo.style.display = 'none'
+    }
+}
+
+function MarkReadToggler(id) {
+    todoList[id].isRead = !todoList[id].isRead;
+    if (todoList[id].isRead) {
+        document.getElementById(id).style.textDecoration = 'line-through'
+    } else {
+        document.getElementById(id).style.textDecoration = 'none'
     }
 }
