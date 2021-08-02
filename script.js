1d179ccf990b6f474cf75ba1eb586125240dc036
thahseen21 <thahseen.niaz@gmail.com>
2021-08-02 15:33:12 +0530

    adding a hardcoded list of item to the html by clicking a button

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+var todoList = ['wash clothes', 'sweeping']
+
+
 function ClickHandler() {
-    console.log("yes working")
+
+    for (var todo of todoList) {
+        const p = document.createElement("p"); // creating a html element as <p> tag
+        p.className = 'todo' // setting the <p> with a classname as 'todo'
+
+        const node = document.createTextNode(todo); // creating a text
+
+        p.appendChild(node); // adding the text to the <p>
+
+        const element = document.getElementById("list-container"); // getting where to insert the <p> tag
+        element.appendChild(p);// appending p tag as child to the "list-container" id.
+    }
 }
