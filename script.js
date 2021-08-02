4ef2f6eb4144edf5ef45a45f0feaeed8155c50ee
thahseen21 <thahseen.niaz@gmail.com>
2021-08-02 17:13:25 +0530

    implemented remove todo from the list

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var todoList = ['wash clothes', 'sweeping']
+var todoList = [];
 
 function AddTodo() {
     const value = document.getElementById("add-todo").value;
@@ -7,16 +7,38 @@ function AddTodo() {
         Display();
         document.getElementById("add-todo").value = null;
     }
+    if (todoList.length > 0) {
+        ToggleNoTodo()
+    }
 }
 
 function Display() {
     const p = document.createElement("p"); // creating a html element as <p> tag
-    p.className = 'todo' // setting the <p> with a classname as 'todo'
+    p.className = `todo` // setting the <p> with a classname as 'todo'
+    p.id = `${todoList.length - 1}`
 
     const node = document.createTextNode(todoList[todoList.length - 1]); // creating a text
 
     p.appendChild(node); // adding the text to the <p>
+    p.addEventListener('click', RemoveTodo)
 
     const element = document.getElementById("list-container"); // getting where to insert the <p> tag
     element.append(p);// appending p tag as child to the "list-container" id.
+}
+
+function RemoveTodo(id) {
+    let idName = id?.target?.id
+    todoList.splice(idName, 1)
+    let tag = document.getElementById(idName);
+    tag.remove();
+    ToggleNoTodo();
+}
+
+function ToggleNoTodo() {
+    const todo = document.getElementById("empty-todo")
+    if (todo?.style?.display === 'none') {
+        todo.style.display = 'block'
+    } else {
+        todo.style.display = 'none'
+    }
 }
