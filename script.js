2b4a460cf3da088a41a8791815fb34fabfa6c9a1
thahseen21 <thahseen.niaz@gmail.com>
2021-08-03 17:11:59 +0530

    new style and added local storage

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var todoList = [];
 
+let keyRef = 0;
+
 function AddTodo() {
     const value = document.getElementById("add-todo").value;
 
@@ -16,20 +18,21 @@ function AddTodo() {
         }
         let temp = { value: value, isRead: false, isRemoved: false }
         todoList.push(temp)
-        Display();
+        AddElementToHTML();
         document.getElementById("add-todo").value = null;
+        updateLocalStorage();
     } else {
         alert('Please enter a todo');
     }
 }
 
-function Display() {
+function AddElementToHTML() {
     const p = document.createElement("p"); // creating a html element as <p> tag
     p.id = `todo-name-${todoList.length - 1}`
 
     const container = document.createElement("div"); // creating a html element as <p> tag
     container.className = `todo` // setting the <p> with a classname as 'todo'
-    container.id = `${todoList.length - 1}`
+    container.id = `${keyRef}`
 
     const removeBtn = document.createElement("button");
     removeBtn.textContent = 'X';
@@ -45,26 +48,37 @@ function Display() {
 
     const element = document.getElementById("list-container"); // getting where to insert the <p> tag
     element.append(container);// appending p tag as child to the "list-container" id.
+    keyRef++;
 }
 
 function RemoveTodo(id) {
     let allRemoved = true;
-    if (todoList[id].isRead) {
-        let tag = document.getElementById(id);
-        todoList[id].isRemoved = true
-        tag.style.display = 'none';
+    // if (todoList[id].isRead) {
+    let tag = document.getElementById(id);
+    console.log(tag.getElementsByTagName('p')[0].innerText)
 
-        for (let todo of todoList) {
-            if (!todo.isRemoved) {
-                allRemoved = false
-            }
+    todoList = todoList.filter(todo => todo.value !== tag.getElementsByTagName('p')[0].innerText)
+
+    console.log(todoList)
+
+
+    // todoList[id].isRemoved = true
+    tag.style.display = 'none';
+
+    for (let todo of todoList) {
+        if (!todo.isRemoved) {
+            allRemoved = false
         }
-        if (allRemoved) {
-            ToggleNoTodo();
-        }
-    } else {
-        alert("Please finish the task to remove...")
     }
+    if (allRemoved) {
+        ToggleNoTodo();
+    }
+    tag.remove();
+
+    updateLocalStorage();
+    // } else {
+    // alert("Please finish the task to remove...")
+    // }
 }
 
 function ToggleNoTodo() {
@@ -84,4 +98,17 @@ function MarkReadToggler(id) {
     } else {
         document.getElementById(`todo-name-${id}`).style.textDecoration = 'none'
     }
+}
+
+function GetListFromLocalStorage() {
+    const data = localStorage.getItem('todoList');
+    if (data) {
+        todoList = JSON.parse(data);
+    } else {
+        console.log('no data found')
+    }
+}
+
+function updateLocalStorage() {
+    localStorage.setItem('todoList', JSON.stringify(todoList))
 }
