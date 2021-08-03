e9a92350141022a77fab24212b0586a30199dddf
thahseen21 <thahseen.niaz@gmail.com>
2021-08-03 18:04:54 +0530

    new method for deleting and inserting value in the todo list

--- script.js
+++ script.js
@@ -7,28 +7,20 @@ function AddTodo() {
 
 
     if (value) {
-        let allRemoved = true;
-        for (let todo of todoList) {
-            if (!todo.isRemoved) {
-                allRemoved = false
-            }
-        }
-        if (todoList.length === 0 || allRemoved) {
-            ToggleNoTodo()
-        }
         let temp = { value: value, isRead: false, isRemoved: false }
         todoList.push(temp)
         AddElementToHTML();
         document.getElementById("add-todo").value = null;
         updateLocalStorage();
+        ToggleNoTodo()
     } else {
         alert('Please enter a todo');
     }
 }
 
-function AddElementToHTML() {
+function AddElementToHTML(todo = null) {
     const p = document.createElement("p"); // creating a html element as <p> tag
-    p.id = `todo-name-${todoList.length - 1}`
+    p.id = `todo-name-${keyRef}`
 
     const container = document.createElement("div"); // creating a html element as <p> tag
     container.className = `todo` // setting the <p> with a classname as 'todo'
@@ -38,7 +30,7 @@ function AddElementToHTML() {
     removeBtn.textContent = 'X';
     removeBtn.addEventListener('click', () => RemoveTodo(container.id))
 
-    const node = document.createTextNode(todoList[todoList.length - 1].value); // creating a text
+    const node = document.createTextNode(todo?.value ?? todoList[todoList.length - 1].value); // creating a text
 
     p.appendChild(node); // adding the text to the <p>
     container.appendChild(p);
@@ -52,38 +44,24 @@ function AddElementToHTML() {
 }
 
 function RemoveTodo(id) {
-    let allRemoved = true;
-    // if (todoList[id].isRead) {
     let tag = document.getElementById(id);
-    console.log(tag.getElementsByTagName('p')[0].innerText)
+    var todo = todoList.filter(todo => todo.value === tag.getElementsByTagName('p')[0].innerText)
+    if (todo[0].isRead) {
 
-    todoList = todoList.filter(todo => todo.value !== tag.getElementsByTagName('p')[0].innerText)
+        todoList = todoList.filter(todo => todo.value !== tag.getElementsByTagName('p')[0].innerText)
+        tag.style.display = 'none';
 
-    console.log(todoList)
-
-
-    // todoList[id].isRemoved = true
-    tag.style.display = 'none';
-
-    for (let todo of todoList) {
-        if (!todo.isRemoved) {
-            allRemoved = false
-        }
-    }
-    if (allRemoved) {
+        tag.remove();
+        updateLocalStorage();
         ToggleNoTodo();
+    } else {
+        alert("Please finish the task to remove...")
     }
-    tag.remove();
-
-    updateLocalStorage();
-    // } else {
-    // alert("Please finish the task to remove...")
-    // }
 }
 
 function ToggleNoTodo() {
     const todo = document.getElementById("empty-todo")
-    if (todo?.style?.display === 'none') {
+    if (todoList.length === 0) {
         todo.style.display = 'block'
     } else {
         todo.style.display = 'none'
@@ -91,9 +69,10 @@ function ToggleNoTodo() {
 }
 
 function MarkReadToggler(id) {
-    todoList[id].isRead = !todoList[id].isRead;
-    const todo = document.getElementById(id)
-    if (todoList[id].isRead) {
+    let tag = document.getElementById(id);
+    let todo = todoList.filter(todo => todo.value === tag.getElementsByTagName('p')[0].innerText)
+    todo[0].isRead = !todo[0].isRead;
+    if (todo[0].isRead) {
         document.getElementById(`todo-name-${id}`).style.textDecoration = 'line-through'
     } else {
         document.getElementById(`todo-name-${id}`).style.textDecoration = 'none'
@@ -104,6 +83,10 @@ function GetListFromLocalStorage() {
     const data = localStorage.getItem('todoList');
     if (data) {
         todoList = JSON.parse(data);
+        for (let todo of todoList) {
+            AddElementToHTML(todo);
+        }
+        ToggleNoTodo();
     } else {
         console.log('no data found')
     }
